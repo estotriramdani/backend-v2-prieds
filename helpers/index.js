9f306744ee50a36edcf02c6222be47b6a1402fe4
Esto Triramdani N <estolagi@gmail.com>
2022-11-24 17:05:15 +0700

    first commit

---
+++ helpers/index.js
@@ -0,0 +1,98 @@
+/**
+ * @function mapPayload
+ * @param {{payload: string}[]} payloads
+ * @return {string[]}
+ */
+const mapPayload = (payloads) => {
+  return payloads.map((item) => item.payload);
+};
+
+/** @typedef {object} MasterData
+ * @property {string} _id
+ * @property {string} company_id
+ * @property {string} scanned_time
+ * @property {string} created_time
+ * @property {string} last_updated
+ * @property {number} qty
+ * @property {any} attribute_list
+ * @property {string} line_id
+ * @property {string} line_type
+ * @property {string} count_string
+ * @property {number} count
+ * @property {string} payload
+ * @property {string} order_no
+ * @property {string} prodis_line_id
+ * @property {string} prodis_line_type
+ * @property {any} qr_list
+ * @property {number} status_repacking
+ * @property {number} status_move_to_bin
+ * @property {number} status
+ * @property {number} status_qc
+ * @property {number} status_sync
+ * @property {string} last_synced
+ * @property {number} status_pick
+ * @property {number} stock_type
+ * @property {number} __v
+ */
+/**
+ * @function changePayloadStatuses
+ * @param {Object} params
+ * @param {MasterData[]} params.masterData - commentParam1
+ * @param {string[]} params.rejects - commentParam2
+ * @return {MasterData[]}
+ */
+const changePayloadStatuses = (params) => {
+  const { masterData, rejects } = params;
+  return masterData.map((data) => {
+    if (rejects.includes(data.payload)) {
+      return {
+        ...data,
+        status: 0,
+        status_qc: 1,
+      };
+    }
+    return data;
+  });
+};
+
+/**
+ * @function changePayloadStatuses
+ * @param {Object} params
+ * @param {MasterData[]} params.masterData - commentParam1
+ * @return {1|0}
+ */
+const finalStatusDecider = (params) => {
+  const { masterData } = params;
+  const zeroStatues = {
+    status: 0,
+    status_qc: 0,
+    status_sync: 0,
+    status_pick: 0,
+  };
+  const roundUp = Math.ceil(masterData.length / 2);
+  const decider = (zero) => {
+    return zero > roundUp ? 0 : 1;
+  };
+  masterData.forEach((data) => {
+    if (data?.status === 0) {
+      zeroStatues.status += 1;
+    }
+    if (data?.status_qc === 0) {
+      zeroStatues.status_qc += 1;
+    }
+    if (data?.status_sync === 0) {
+      zeroStatues.status_sync += 1;
+    }
+    if (data?.status_pick === 0) {
+      zeroStatues.status_pick += 1;
+    }
+  });
+  return {
+    status: decider(zeroStatues.status),
+    status_qc: decider(zeroStatues.status_qc),
+    status_sync: decider(zeroStatues.status_sync),
+    status_pick: decider(zeroStatues.status_pick),
+  };
+};
+
+module.exports = { changePayloadStatuses, finalStatusDecider, mapPayload };
---
+++ routes/index.js
@@ -0,0 +1,72 @@
+var express = require('express');
+var router = express.Router();
+const stock_read_log = require('../models/stock_read_log');
+const FileSystem = require('fs');
+const { changePayloadStatuses, mapPayload, finalStatusDecider } = require('../helpers');
+
+router.use('/export-data', async (req, res) => {
+  const list = await stock_read_log
+    .aggregate([
+      {
+        $match: {},
+      },
+    ])
+    .exec();
+
+  FileSystem.writeFile('./stock_read_log.json', JSON.stringify(list), (error) => {
+    if (error) throw error;
+  });
+
+  console.log('stock_read_log.json exported!');
+  res.json({ statusCode: 1, message: 'stock_read_log.json exported!' });
+});
+
+router.use('/import-data', async (req, res) => {
+  const list = await stock_read_log
+    .aggregate([
+      {
+        $match: {},
+      },
+    ])
+    .exec();
+
+  FileSystem.readFile('./stock_read_log.json', async (error, data) => {
+    if (error) throw error;
+
+    const list = JSON.parse(data);
+
+    const deletedAll = await stock_read_log.deleteMany({});
+
+    const insertedAll = await stock_read_log.insertMany(list);
+
+    console.log('stock_read_log.json imported!');
+    res.json({ statusCode: 1, message: 'stock_read_log.json imported!' });
+  });
+});
+
+router.use('/edit-repacking-data', async (req, res) => {
+  const { reject_qr_list, new_qr_list, company_id, payload } = req.body;
+  const rejects = mapPayload(reject_qr_list);
+  const payloads = [...mapPayload(reject_qr_list), ...mapPayload(new_qr_list)];
+  const stocks = await stock_read_log
+    .find({
+      payload: { $in: payloads },
+    })
+    .lean();
+  const updatedStatuses = changePayloadStatuses({ masterData: stocks, rejects });
+  const finalStatus = finalStatusDecider({ masterData: stocks });
+  const responseData = {
+    qty: updatedStatuses.length,
+    qr_list: updatedStatuses,
+    company_id,
+    payload,
+    ...finalStatus,
+  };
+  res.status(201).json(responseData);
+});
+
+router.use('/', function (req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
